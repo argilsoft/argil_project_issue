61666f7459b0e903b302a527ec3e911f2d31c9aa
Israel CA <israel.cruz.argil@gmail.com>
2018-01-01 18:33:21 -0600

    Update

---
+++ argil_project_stages/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name"          : "Stages in Project Form View",
+    "version"       : "1.0",
+    "author"        : "Argil Consulting",
+    "category"      : "Project",
+    "description"   : """
+    This module adds relation with Stages in PRoject Form View
+""",
+    "website"       : "http://www.argil.mx",
+    #"license" : "AGPL-3",
+    "depends"       : ["project",
+        ],
+    "data"          : ['project_view.xml'],
+    "installable"   : True,
+}
