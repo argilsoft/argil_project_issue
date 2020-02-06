b9603fc8b643127c544efe935eef23dec7e35614
Israel CA <israel.cruz.argil@gmail.com>
2020-02-05 19:07:15 -0600

    IMP - Se agrega al portal el numero de Ticket

--- helpdesk_mgmt_eta/models/helpdesk_ticket.py
+++ helpdesk_mgmt_eta/models/helpdesk_ticket.py
@@ -5,3 +5,5 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     date_expected = fields.Date(string="Fecha Estimada")
+    commercial_partner_id = fields.Many2one('res.partner', related='partner_id.commercial_partner_id',
+                                           readonly=True, store=True)
